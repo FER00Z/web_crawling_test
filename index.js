6d681f808056044c3a2655bda7695c5f2e5ce946
Feruz <ferooz.it@gmail.com>
2022-06-23 21:09:10 +0200

    some_updates

---
+++ index.js
@@ -0,0 +1,62 @@
+// import * as fetch from "node-fetch";
+// import * as cheerio from "cheerio";
+// import * as path from "path";
+// import * as urlParser from "url";
+
+const fs = require('fs'); 
+const fetch = require('node-fetch'); 
+const cheerio = require('cheerio'); 
+const urlParser = require('url'); 
+
+const seenUrls = {};
+
+const getUrl = (link, host, protocol) => {
+  if (link.includes("http")) {
+    return link;
+  } else if (link.startsWith("/")) {
+    return `${protocol}//${host}${link}`;
+  } else {
+    return `${protocol}//${host}/${link}`;
+  }
+};
+
+const crawl = async ({ url, ignore }) => {
+  if (seenUrls[url]) return;
+  console.log("crawling", url);
+  seenUrls[url] = true;
+
+  const { host, protocol } = urlParser.parse(url);
+
+  const response = await fetch(url);
+  const html = await response.text();
+  const $ = cheerio.load(html);
+  const links = $("a")
+    .map((i, link) => link.attribs.href)
+    .get();
+
+  //   const imageUrls = $("img")
+  //   .map((i, link) => link.attribs.src)
+  //   .get();
+
+  // imageUrls.forEach((imageUrl) => {
+  //   fetch(getUrl(imageUrl, host, protocol)).then((response) => {
+  //     const filename = path.basename(imageUrl);
+  //     const dest = fs.createWriteStream(`images/${filename}`);
+  //     response.body.pipe(dest);
+  //   });
+  // });
+
+  links
+    .filter((link) => link.includes(host) && !link.includes(ignore))
+    .forEach((link) => {
+      crawl({
+        url: getUrl(link, host, protocol),
+        ignore,
+      });
+    });
+};
+
+crawl({
+  url: "https://ipfabric.io/",
+  ignore: "/search",
+});
